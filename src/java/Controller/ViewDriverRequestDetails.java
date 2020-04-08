3f5894e252bbf36673d9c197de7694791bfb9290
AyahAL28 <37771547+AyahAL28@users.noreply.github.com>
2020-04-09 00:19:42 +0400

    Create ViewDriverRequestDetails.java

---
+++ src/java/Controller/ViewDriverRequestDetails.java
@@ -0,0 +1,7 @@
+//View Driver request details
+/* session.setAttribute("username", userName);
+                //If admin, go to the view driver applications page
+                if (Admin.isAdmin(userName)) {
+                    RequestDispatcher rd = request.getRequestDispatcher("ViewDriverApplications.jsp");
+                    rd.forward(request, response);
+                }*/
